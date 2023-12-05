089ad98247a06da8c0ab969d920fd40b9c993091
Nana Brempong <student.157163@worcesterschools.net>
2023-12-05 17:06:11 -0500

    first

---
+++ script.js
@@ -0,0 +1,21 @@
+let x =5;
+
+console.log(x)
+
+let a = [56,76,89,2,0]
+
+console.log(a)
+
+console.log(a.length)
+
+a.push(a)
+console.log(a[100])
+
+let b = ["car", "`bike`", "truck"]
+
+console.log(b)
+console.log(b[1])
+
+for (let i = 0; i <b.length; i++) {
+    console.log('foo', i)
+}
